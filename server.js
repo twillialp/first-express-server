093f4014cf8280b280bcaf24790fce9708e65e96
Addy Williams <awillia5@gmail.com>
2025-11-22 17:57:02 -0800

    finally done with this super simple prorject that was due a week ago :(

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const axios = require('axios');
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = 4000;
  
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,6 +11,24 @@ app.get("/", (request, response) => {
  
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+//daily fact portion
+app.get('/api/fun-fact', async (req, res) => {
+  try {
+    const response = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random');
+
+    res.json(response.data.text);
+  } catch (error) {
+
+    if (error.response) {
+      console.error('API Error:', error.response.status, error.response.data);
+      res.status(error.response.status).json({ message: 'Error fetching data from external API.' });
+    } else {
+      console.error('Network Error:', error.message);
+      res.status(500).json({ message: 'A network error occurred.' });
+    }
+  }
 });
 
 app.listen(PORT, () => {
